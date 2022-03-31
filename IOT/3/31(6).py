a0cedf6486dcb883e1554dc38a99cef07e10dcf7
Kim Tae Hyun <97386129+tjdrkr2580@users.noreply.github.com>
2022-03-31 10:41:12 +0900

    IOT Python 개발2

--- IOT/3/31(6).py
+++ IOT/3/31(6).py
@@ -12,3 +12,19 @@ print("a * 3 = ", a * 3)
 
 print("리스트 길이")
 print("a 길이: ",len(a))
+
+array = [0,1,2]
+array + [10,11]
+array.append(12)
+array.append([20,21])
+array.insert(2,100)
+array.extend([30,31])
+print(array)
+
+
+
+lia = [0,1,2,3,4,5]
+lia.sort()
+print(lia)
+lia.reverse()
+print(lia)
---
+++ IOT/3/31(7).py
@@ -0,0 +1,12 @@
+for i in range(10):
+    print('정보통신')
+
+array = [1,2,3,4,5,6]
+
+for nums in array:
+    print(nums)
+
+array = list(range(3))
+
+for elements in array:
+    print(elements)
