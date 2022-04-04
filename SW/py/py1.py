cb34267b536b985eedce4bd9afe8d00c3b650a37
Kim Tae Hyun <97386129+tjdrkr2580@users.noreply.github.com>
2022-04-04 12:52:06 +0900

    SW python 1

---
+++ SW/py/py1.py
@@ -0,0 +1,12 @@
+l = []
+player = ["Messi" , 10 , True]
+list("happy")
+list((1125,2436))
+list(("사과","배"))
+nums = list(range(3))
+list = ["뱀","호랑이","낙타","토끼","사자","바퀴벌레","미어캣","앵무새"];
+list.pop()
+print(list)
+list.pop()
+list.pop()
+list.pop()
