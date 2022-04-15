e47d4707c8e3585f3c8bb5435768d22866f01cc1
Kim Tae Hyun <97386129+tjdrkr2580@users.noreply.github.com>
2022-04-15 13:56:28 +0900

    SW html prac 6~ sementic

--- SW/py/py1.py
+++ SW/py/py1.py
@@ -1,12 +1,35 @@
-l = []
-player = ["Messi" , 10 , True]
-list("happy")
-list((1125,2436))
-list(("사과","배"))
-nums = list(range(3))
-list = ["뱀","호랑이","낙타","토끼","사자","바퀴벌레","미어캣","앵무새"];
-list.pop()
-print(list)
-list.pop()
-list.pop()
-list.pop()
+from re import X
+
+
+t = ()
+xy = (2560,1440)
+color= 129,247,216
+print(xy + color)
+print(xy * 2)
+
+color = 129,247,216
+red , green , blue = color
+print(red)
+print(green)
+print(blue)
+type(red)
+
+x = 2560
+y = 1440
+x , y = y , x
+print(x)
+print(y)
+
+
+a = (123,"abc",True,123)
+print(a[1])
+print(a[2:])
+print(a[:2])
+
+s3 = {3,6,9,12}
+s4 = {4,8,12,16}
+print(s3 & s4) 
+
+a = True
+b = False
+print(type(a))
