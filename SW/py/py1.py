1287628b3bb116b31b5a3e1d25b76db90b91fb5a
Kim Tae Hyun <97386129+tjdrkr2580@users.noreply.github.com>
2022-06-08 12:37:02 +0900

    ~ practice 30_2

--- SW/py/py1.py
+++ SW/py/py1.py
@@ -1,35 +1,20 @@
-from re import X
+numbers = [1,5,-2,0,6]
+print(numbers, "중 가장 큰 값은",max(numbers))
+print(numbers,"중 가장 작은 값은 ",min(numbers))
+print(numbers, "합계는 ",sum(numbers))
+print("2의 10승은", pow(2,10))
 
 
-t = ()
-xy = (2560,1440)
-color= 129,247,216
-print(xy + color)
-print(xy * 2)
-
-color = 129,247,216
-red , green , blue = color
-print(red)
-print(green)
-print(blue)
-type(red)
-
-x = 2560
-y = 1440
-x , y = y , x
-print(x)
-print(y)
+pi = 3.14152
+print(pi,"의 소수점 1자리 반올림은", round(pi))
+print(pi,"의 소수점 1자리 반올림은", round(pi,0))
+print(pi,"의 소수점 2자리 반올림은", round(pi,1))
+print(pi,"의 소수점 3자리 반올림은", round(pi,2))
+print(pi,"의 소수점 4자리 반올림은", round(pi,3))
 
 
-a = (123,"abc",True,123)
-print(a[1])
-print(a[2:])
-print(a[:2])
-
-s3 = {3,6,9,12}
-s4 = {4,8,12,16}
-print(s3 & s4) 
-
-a = True
-b = False
-print(type(a))
+user_name = input("이름은? ")
+user_age = input("나이는? ")
+print(user_name+"님! 나이는 " +str(user_age) + "세군요!")
+say = "{0}님! 나이는 {1}세라니 놀라워요!"
+print(say.format(user_name,user_age))
